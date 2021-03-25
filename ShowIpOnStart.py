7e068aba92c1c17391eb125b965509e0c99caae7
VanBreC <xbox15497@hotmail.com>
2021-03-25 18:15:05 -0500

    Added message to be displayed while connecting to wifi

--- ShowIpOnStart.py
+++ ShowIpOnStart.py
@@ -6,12 +6,16 @@ import subprocess
 
 Loop = True
 
+
 #To run on start up, enter command sudo crontab -e
 #Choose nano
 #Add to end of file @reboot python3 /home/pi/Documents/IoTProject2021/ShowIpOnStart.py
 while Loop:
     res = str(subprocess.check_output(['hostname', '-I'])).split(' ')[0].replace("b'", "")
-    sense.show_message(res)
+    if res[0:3] != "192":
+        sense.show_message("Connecting...", scroll_speed=0.05)
+    else:
+        sense.show_message(res)
     #print (res.split(".")[3])
     for event in sense.stick.get_events():
         if event.direction == "middle" and (event.action == "pressed" or event.action == "held"):
