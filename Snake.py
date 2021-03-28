4fe3197e542c89eb8f637c832b05b53143dedd28
VanBreC <xbox15497@hotmail.com>
2021-03-28 03:52:22 -0500

    Added capability to connect to and send json data to database

--- Snake.py
+++ Snake.py
@@ -2,8 +2,30 @@ from sense_hat import SenseHat
 sense = SenseHat()
 from time import sleep
 from random import choice
+import paho.mqtt.client as mqtt
+import json
+import subprocess
 
 sense.clear()
+
+broker_address="192.168.1.32"
+sship = str(subprocess.check_output(['hostname', '-I'])).split(' ')[0].replace("b'", "")
+endofsship = sship.split(".")[3]
+client = mqtt.Client("DWF"+endofsship)
+
+g = (0, 255, 0)
+e = (0, 0, 0)
+
+check_mark = [
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,g,
+e,e,e,e,e,e,g,e,
+e,e,e,e,e,g,e,e,
+g,e,e,e,g,e,e,e,
+e,g,e,g,e,e,e,e,
+e,e,g,e,e,e,e,e
+]
 
 GameStart = False
 Wait = True
@@ -58,8 +80,8 @@ while GameStart:
         SnakeBodyy.append(SnakeHeady)
         SnakeBodyPos.append(int(str(SnakeHeadx)+str(SnakeHeady)))
         if (SnakeBodyPos.count(int(str(SnakeHeadx)+str(SnakeHeady))) > 1) or (SnakeHeadx < 0 or SnakeHeadx > 7) or (SnakeHeady < 0 or SnakeHeady > 7):
-            sense.show_message("Game Over", scroll_speed=0.075)
-            sense.show_message("Your Score was " + str(Score), scroll_speed=0.075)
+            sense.show_message("Game Over", scroll_speed=0.05)
+            sense.show_message("Your Score was " + str(Score), scroll_speed=0.05)
             GameStart = False
             break
         sense.set_pixel(SnakeHeadx, SnakeHeady, (0, 255, 0))
@@ -80,4 +102,84 @@ while GameStart:
                 Fruity = choice([0, 1, 2, 3, 4, 5, 6, 7])
                 FruitPos = int(str(Fruitx)+str(Fruity))
             sense.set_pixel(Fruitx, Fruity, (255, 0, 0))
-        sleep(1)
+        sleep(0.5)
+
+sense.show_message("Save your score?", scroll_speed=0.04, text_colour=[0,0,255])
+sense.show_message("Yes (U)", scroll_speed=0.04, text_colour=[0,255,0])
+sense.show_message("No (D)", scroll_speed=0.04, text_colour=[255,0,0])
+
+for event in sense.stick.get_events():
+    continue
+
+sense.show_letter("Y", text_colour=[0,255,0])
+Keep = "Y"
+
+LeaderBoard = False
+Save = True
+
+while Save:
+    for event in sense.stick.get_events():
+        if event.direction == "up":
+            sense.show_letter("Y", text_colour=[0,255,0])
+            Keep = "Y"
+        elif event.direction == "down":
+            sense.show_letter("N", text_colour=[255,0,0])
+            Keep = "N"
+        elif event.direction == "middle":
+            if Keep == "Y":
+                sense.show_message("Select Three Characters", scroll_speed=0.04, text_colour=[0,0,255])
+                LeaderBoard = True
+                name = ""
+                i = 65
+                sense.show_letter(chr(i))
+                Save = False
+            elif Keep == "N":
+                sense.show_message("Thanks For Playing!!!", scroll_speed=0.04, text_colour=[0,0,255])
+                Save = False
+            else:
+                continue
+
+
+while LeaderBoard:
+    if i==91:
+        i = 48
+    elif i==58:
+        i = 65
+    elif i == 64:
+        i = 57
+    elif i == 47:
+        i = 90
+    elif len(name)==3:
+        sense.show_message(name + " " + str(Score), scroll_speed=0.04, text_colour=[255,125,0])
+        sense.show_message("Thanks For Playing!!!", scroll_speed=0.04, text_colour=[0,0,255])
+        info = json.dumps({"Game": "Snake", "ID": name, "Score": Score})
+        #print (info)
+        sleep(0.25)
+        try:
+            client.connect(broker_address)
+        except OSError:
+            print("Failed to connect to Server | Attempting connection to local VM")
+            broker_address="192.168.1.10"
+            client.connect(broker_address)
+        except:
+            print("Both Connections Failed")
+            LeaderBoard = False
+            break
+        sleep(0.25)
+        client.publish("GameScores", info)
+        LeaderBoard = False
+    else:
+        character = chr(i)
+        sense.show_letter(character)
+    for event in sense.stick.get_events():
+
+        if event.direction == "up" and (event.action == "pressed" or event.action == "held"):
+            i = i+1
+
+        elif event.direction == "down" and (event.action == "pressed" or event.action == "held"):
+            i = i-1
+
+        elif event.direction == "middle" and event.action == "pressed":
+            name = name + character
+            sense.set_pixels(check_mark)
+            sleep(0.5)
