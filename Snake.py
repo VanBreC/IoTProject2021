6b1fc7923a76d53ee19e7e2994623a85530ea0ed
VanBreC <xbox15497@hotmail.com>
2021-03-30 19:13:42 -0500

    Fixed some bugs and editing LED brightness

--- Snake.py
+++ Snake.py
@@ -13,7 +13,10 @@ sship = str(subprocess.check_output(['hostname', '-I'])).split(' ')[0].replace("
 endofsship = sship.split(".")[3]
 client = mqtt.Client("DWF"+endofsship)
 
+o = (200,110,0)
 g = (0, 255, 0)
+r = (255,0,0)
+b = (0,0,200)
 e = (0, 0, 0)
 
 check_mark = [
@@ -42,76 +45,92 @@ Fruitx = 6
 Fruity = 3
 
 #sense.set_pixel(column, row, (r, g, b))
-sense.set_pixel(SnakeHeadx, SnakeHeady, (0, 255, 0))
-sense.set_pixel(Fruitx, Fruity, (255, 0, 0))
+sense.set_pixel(SnakeHeadx, SnakeHeady, g)
+sense.set_pixel(Fruitx, Fruity, r)
 
 while Wait:
     for event in sense.stick.get_events():
         if event.action == "pressed":
             Direction = str(event.direction)
-            PrevDirection = ""
             #print (Direction)
             GameStart = True
             Wait = False
 
 while GameStart:
+    if Score == 64:
+        sense.show_message("Perfect Snake!!!", scroll_speed=0.05)
+        sense.show_message("Your Score was " + str(Score), scroll_speed=0.05)
+        GameStart = False
+        break
+    DirectionList = []
     for event in sense.stick.get_events():
         if event.action == "pressed":
-            PrevDirection = Direction
-            Direction = str(event.direction)
+            DirectionList.append(event.direction)
+            if (DirectionList[0] == "right") and (Direction != "left"):
+                Direction = DirectionList[0]
+            elif (DirectionList[0] == "left") and (Direction != "right"):
+                Direction = DirectionList[0]
+            elif (DirectionList[0] == "up") and (Direction != "down"):
+                Direction = DirectionList[0]
+            elif (DirectionList[0] == "down") and (Direction != "up"):
+                Direction = DirectionList[0]
+            else:
+                continue
 
     x = 0
     y = 0
-    if (Direction == "right") and (PrevDirection != "left"):
+    if (Direction == "right"):
         x = x+1
-    elif (Direction == "left") and (PrevDirection != "right"):
+    elif (Direction == "left"):
         x = x-1
-    elif (Direction == "up") and (PrevDirection != "down"):
+    elif (Direction == "up"):
         y = y-1
-    elif (Direction == "down") and (PrevDirection != "up"):
+    elif (Direction == "down"):
         y = y+1
     else:
-        Direction = PrevDirection
+        continue
 
-    if x or y != 0:
+
+
+    if x or y == 1 or -1:
         SnakeHeadx = SnakeHeadx+x
         SnakeBodyx.append(SnakeHeadx)
         SnakeHeady = SnakeHeady+y
         SnakeBodyy.append(SnakeHeady)
-        SnakeBodyPos.append(int(str(SnakeHeadx)+str(SnakeHeady)))
-        if (SnakeBodyPos.count(int(str(SnakeHeadx)+str(SnakeHeady))) > 1) or (SnakeHeadx < 0 or SnakeHeadx > 7) or (SnakeHeady < 0 or SnakeHeady > 7):
+        #print ("X: ", SnakeHeadx, "\nY: ", SnakeHeady)
+        SnakeBodyPos.append(str(SnakeHeadx)+str(SnakeHeady))
+        if (SnakeBodyPos.count(str(SnakeHeadx)+str(SnakeHeady)) > 1) or (SnakeHeadx < 0 or SnakeHeadx > 7) or (SnakeHeady < 0 or SnakeHeady > 7):
             sense.show_message("Game Over", scroll_speed=0.05)
             sense.show_message("Your Score was " + str(Score), scroll_speed=0.05)
             GameStart = False
             break
-        sense.set_pixel(SnakeHeadx, SnakeHeady, (0, 255, 0))
+        sense.set_pixel(SnakeHeadx, SnakeHeady, g)
         sense.set_pixel(SnakeBodyx[0], SnakeBodyy[0], (0, 0, 0))
-        if Score < (len(SnakeBodyx) and len(SnakeBodyy)):
-            del SnakeBodyx[0]
-            del SnakeBodyy[0]
-            del SnakeBodyPos[0]
+        del SnakeBodyx[0]
+        del SnakeBodyy[0]
+        del SnakeBodyPos[0]
 
         if SnakeHeadx == Fruitx and SnakeHeady == Fruity:
             Score = Score+1
             SnakeBodyx.insert(1,SnakeBodyx[0])
             SnakeBodyy.insert(1,SnakeBodyy[0])
             SnakeBodyPos.insert(1,SnakeBodyPos[0])
-            FruitPos = int(str(Fruitx)+str(Fruity))
+            FruitPos = str(Fruitx)+str(Fruity)
             while FruitPos in SnakeBodyPos:
                 Fruitx = choice([0, 1, 2, 3, 4, 5, 6, 7])
                 Fruity = choice([0, 1, 2, 3, 4, 5, 6, 7])
-                FruitPos = int(str(Fruitx)+str(Fruity))
-            sense.set_pixel(Fruitx, Fruity, (255, 0, 0))
+                FruitPos = str(Fruitx)+str(Fruity)
+            sense.set_pixel(Fruitx, Fruity, r)
         sleep(0.5)
 
-sense.show_message("Save your score?", scroll_speed=0.04, text_colour=[0,0,255])
-sense.show_message("Yes (U)", scroll_speed=0.04, text_colour=[0,255,0])
-sense.show_message("No (D)", scroll_speed=0.04, text_colour=[255,0,0])
+sense.show_message("Save your score?", scroll_speed=0.04, text_colour=[0,0,127])
+sense.show_message("Yes (U)", scroll_speed=0.04, text_colour=[0,127,0])
+sense.show_message("No (D)", scroll_speed=0.04, text_colour=[127,0,0])
 
 for event in sense.stick.get_events():
     continue
 
-sense.show_letter("Y", text_colour=[0,255,0])
+sense.show_letter("Y", text_colour=[0,127,0])
 Keep = "Y"
 
 LeaderBoard = False
@@ -120,21 +139,21 @@ Save = True
 while Save:
     for event in sense.stick.get_events():
         if event.direction == "up":
-            sense.show_letter("Y", text_colour=[0,255,0])
+            sense.show_letter("Y", text_colour=[0,127,0])
             Keep = "Y"
         elif event.direction == "down":
-            sense.show_letter("N", text_colour=[255,0,0])
+            sense.show_letter("N", text_colour=[127,0,0])
             Keep = "N"
         elif event.direction == "middle":
             if Keep == "Y":
-                sense.show_message("Select Three Characters", scroll_speed=0.04, text_colour=[0,0,255])
+                sense.show_message("Select Three Characters", scroll_speed=0.04, text_colour=[0,0,127])
                 LeaderBoard = True
                 name = ""
                 i = 65
                 sense.show_letter(chr(i))
                 Save = False
             elif Keep == "N":
-                sense.show_message("Thanks For Playing!!!", scroll_speed=0.04, text_colour=[0,0,255])
+                sense.show_message("Thanks For Playing!!!", scroll_speed=0.04, text_colour=[0,0,127])
                 Save = False
             else:
                 continue
@@ -150,8 +169,8 @@ while LeaderBoard:
     elif i == 47:
         i = 90
     elif len(name)==3:
-        sense.show_message(name + " " + str(Score), scroll_speed=0.04, text_colour=[255,125,0])
-        sense.show_message("Thanks For Playing!!!", scroll_speed=0.04, text_colour=[0,0,255])
+        sense.show_message(name + " " + str(Score), scroll_speed=0.04, text_colour=[127,127,0])
+        sense.show_message("Thanks For Playing!!!", scroll_speed=0.04, text_colour=[0,0,127])
         info = json.dumps({"Game": "Snake", "ID": name, "Score": Score})
         #print (info)
         sleep(0.25)
